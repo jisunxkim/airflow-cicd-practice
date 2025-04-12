807d336e93f5eb23f4797217c644310455ad26bb
Jisun Kim <jisunxkim@gmail.com>
2025-04-12 15:45:39 -0700

    added dag to dev

---
+++ dags/dev/simple_dag_dev.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
+
+def print_hello():
+    print("👋 Hello from the DEV DAG!")
+
+with DAG(
+    dag_id='simple_dag__dev',
+    start_date=datetime(2023, 1, 1),
+    schedule_interval='@daily',
+    catchup=False
+) as dag:
+
+    task = PythonOperator(
+        task_id='say_hello',
+        python_callable=print_hello
+    )
